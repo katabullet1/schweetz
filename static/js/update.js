d89ad8e20e235dba2cf271849f0552b88f46cd89
katabullet1 <katabullet2pk@gmail.com>
2024-08-31 15:22:46 +0100

    Add files via upload

---
+++ static/js/update.js
@@ -0,0 +1,10 @@
+$(window).load(function() {
+    $("#contactMSG").click(function(){
+     $("#myModal").modal();
+  });
+    var d = new Date();
+    var n = d.getUTCFullYear();
+    
+    document.getElementById("copyRdate").innerHTML = n;
+    
+});
